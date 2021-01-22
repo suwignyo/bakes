ec87b9d19bf7cf6d097913b8e77cd050423a967b
suwignyo <gsuwignyo@gmail.com>
2021-01-22 18:10:42 -0500

    11. init prisma

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,3 +1,8 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-export {};
+const prisma = new PrismaClient({
+  // log: process.env.NODE_ENV === ["query", "info", "warn"] : [],
+  log: ["query", "info", "warn"],
+});
+
+export { prisma, PrismaClient };
