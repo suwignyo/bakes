0cc230e2d1456f01fdb3978b1ba09e8198525b05
suwignyo <gsuwignyo@gmail.com>
2021-01-25 21:43:56 -0500

    24 added search box in map

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -5,7 +5,7 @@ import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useLocalState } from "src/utils/useLocalState";
 import { CakesQuery_cakes } from "src/generated/CakesQuery";
-// import { SearchBox } from "./searchBox";
+import { SearchBox } from "./searchBox";
 
 interface IProps {
   setDataBounds: (bounds: string) => void;
@@ -47,6 +47,25 @@ export default function Map({ setDataBounds, cakes, highlightedId }: IProps) {
           }
         }}
       >
+        <div className="absolute top-0 w-full z-10 p-4">
+          <SearchBox
+            defaultValue=""
+            onSelectAddress={(_address, latitude, longitude) => {
+              if (latitude && longitude) {
+                setViewport((old) => ({
+                  ...old,
+                  latitude,
+                  longitude,
+                  zoom: 12,
+                }));
+                if (mapRef.current) {
+                  const bounds = mapRef.current.getMap().getBounds();
+                  setDataBounds(JSON.stringify(bounds.toArray()));
+                }
+              }
+            }}
+          ></SearchBox>
+        </div>
         {cakes.map((cake) => {
           return (
             <Marker
